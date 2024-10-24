24c99ca8f179c673790398795391afd343462634
Maciej <swmikolaj909@gmail.com>
2024-10-24 17:49:04 +0200

    base.c added, still creating

--- CS.c
+++ CS.c
@@ -1,27 +1,22 @@
-// THIS IS PROTOTYPE CODE OF GROUND STATION - PROBABLY WILL CHANGE 1000 TIMES,
-// THIS IS ONLY PROTOTYPE
+// THIS IS PROTOTYPE CODE OF CANSAT - PROBABLY WILL CHANGE 1000 TIMES, THIS IS
+// ONLY PROTOTYPE
 #include <netinet/in.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
-#include <stdlib.h> //all those libraries are useless, but first i will write this, than i will comment them out
+#include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
-//
 struct data {
   int temperature;
   int pressure;    // will be transformed to height above sea level
   int latitude;    // geographical latitude
   int geoaltitude; // geographical altitude
 } data;
-// data in the same order as in CS.c
+// data must be in the same order as in base.c
 int main() {
 
   ///
   return 0;
 };
-/*TODO
- * data will be sent and parsed as this struct
- *
- */
