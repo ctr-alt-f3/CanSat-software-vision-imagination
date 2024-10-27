fcc68989ffa95aca9d9874a5f9655e94ffbf166e
ctr-alt-f3 <swmikolaj909@gmail.com>
2024-10-27 16:30:15 +0100

    made code more readable

--- CS.c
+++ CS.c
@@ -16,27 +16,6 @@
 #define RADIO_FREQ ?
 #define RADIO_POWER ? 
 #define RADIO_BANDWITH ?
-/*enum instructions{
-//all opcodes for sendInstruction function will be here
-//TODO:instructions are in bad order, 
-OP_SEND_PHOTO = 2,   //will send the photo from camera
-OP_SEND_GPS = 3,     //will send GPS coordinates
-OP_DEC_FREQ_OF_DATA_SENDING = 4,  //will decrease frequency of sending data (for example when on ground)
-OP_CUT_THE_POWER_CAMERA = 6,
-OP_CUT_THE_POWER_TEMPERATURE_SENSOR = 7,
-OP_CUT_THE_POWER_GPS = 8,
-OP_CUT_THE_POWER_ACCELEROMETR = 9, //and compass, etc.
-//DANGEROUS ACTIONS
-OP_CUT_THE_POWER_ALL = 0, //WILL COMPLETLY POWER OFF THE CANSAT - idk if it's good to make it's opcode "0"
-		OP_CUT_THE_POWER_RADIO = 1,  //WILL COMPLETLY CUT OFF THE COMMUNICATION - dangerous
-//DANGEROUS ACTIONS		
-OP_INC_FREQ_OF_DATA_SENDING = 5, //normally 1 per second
-OP_SEND_BATTERY_STATS = 6,
-//DANGEROUS ACTION
-OP_CHANGE_RADIO_STATS = 7
-
-};*/
-
 unsigned short HASL; // height above sea level
 
 char senddata() {
--- base.c
+++ base.c
@@ -13,28 +13,6 @@
 #include <string.h>
 #include <sys/socket.h> //won't be used for radio communication, but before i do this, i will propably simulate this by network
 #include <unistd.h>
-/*enum instructions{
-//all opcodes for sendInstruction function will be here - TODO
-OP_SEND_PHOTO = 2,   //will send the photo from camera
-OP_SEND_GPS = 3,     //will send GPS coordinates
-OP_DEC_FREQ_OF_DATA_SENDING = 4,  //will decrease frequency of sending data (for example when on ground)
-OP_CUT_THE_POWER_CAMERA = 6,
-OP_CUT_THE_POWER_TEMPERATURE_SENSOR = 7,
-OP_CUT_THE_POWER_GPS = 8,
-OP_CUT_THE_POWER_ACCELEROMETR = 9, //and compass, etc.
-//DANGEROUS ACTIONS
-OP_CUT_THE_POWER_ALL = 0, //WILL COMPLETLY POWER OFF THE CANSAT - idk if it's good to make it's opcode "0"
-		OP_CUT_THE_POWER_RADIO = 1,  //WILL COMPLETLY CUT OFF THE COMMUNICATION - dangerous
-//DANGEROUS ACTIONS		
-OP_INC_FREQ_OF_DATA_SENDING = 5, //normally 1 per second
-OP_SEND_BATTERY_STATS = 6,
-//DANGEROUS
-OP_CHANGE_RADIO_STATS = 7,// power/freq/bandwith
-OP_STOP_ADDIONAL_MISSION = 8, //DANGEROUS, NEVER USE THIS
-OP_POWER_BUTTON_LED_CHANGE = 9,
-OP_RESEND_LAST_INSTRUCTION = 10 //when data transferring isn't ideal, and CHKSUM is invalid
-};*/
-
 
 
 char sendInstruction(__int64_t instruction) { /*128 or 64???*/
